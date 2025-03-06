f2f5b1b63ddde192ab37fe3262f14265e96fe4a4
Tadiwos <tadiwosadanefa@gmail.com>
2025-03-06 11:00:28 +0100

    Update solved merge conflict

--- HelloWorld/src/main/java/org/example/HelloWorld.java
+++ HelloWorld/src/main/java/org/example/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from GitHub!");
+        System.out.println("Hello from both GitHub and feature branch!");
     }
 }
