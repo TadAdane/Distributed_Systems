4f296e8cbd275cd4b08918d0df74ba53a5feb8d3
Edward <edward.avetisyan@student.uantwerpen.be>
2025-03-26 18:02:26 +0100

    Edward - Added Hash function

--- Lab_3/NamingServer/src/main/java/com/example/NamingServer/controller/NodeController.java
+++ Lab_3/NamingServer/src/main/java/com/example/NamingServer/controller/NodeController.java
@@ -12,12 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NodeController {
 
+    private static final int MAX = Integer.MAX_VALUE; // 2_147_483_647
+    private static final int MIN = -Integer.MAX_VALUE; // -2_147_483_647
+
+    // Hashing function (node name -> 0 to 32768)
+    private int hashNodeName(String nodeName) {
+        int hash = nodeName.hashCode(); // can be negative
+        return (int) (((long) hashCode + MAX) * 32768 / ((long) MAX + Math.abs((long) MIN)));
+    }
+
     // Sample endpoint to add a node
     @PostMapping("/addNode")
     public String addNode(@RequestBody Node node) {
+        int hash = hashNodeName(node.getName());
+
         // Here we would handle adding the node to the map
         // For now, just return a simple response
-        return "Node added: " + node.getName();
+        return "Node added: " + node.getName() + " (hash: " + hash + ")";
     }
 
     // Sample endpoint to get the IP address of a file
