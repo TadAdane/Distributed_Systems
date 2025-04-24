68b0092ee7b818ddac680a20d6260f64fcb69029
chrisElh <christian-eh@hotmail.com>
2025-04-24 21:39:19 +0200

    Gestart met NodeApp

--- Lab_3/NamingServer/src/main/java/NodeApp.java
+++ Lab_3/NamingServer/src/main/java/NodeApp.java
@@ -1,17 +1,22 @@
 import NodePackage.Node;
+import network.MulticastSender;
 
 public class NodeApp {
 
-
-
-    public Node createExternalNode(String name , String ipAddress) {
-        Node newNode = new Node(ipAddress, name);
-        return newNode;
+    // Maakt een Node object aan met naam en IP-adres
+    public Node createExternalNode(String name, String ipAddress) {
+        return new Node(ipAddress, name);
     }
 
-
+    // Verstuurt de Node via multicast naar het netwerk
     public void sendMulticast(Node multicastNode) {
-
+        try {
+            // Roep je bestaande MulticastSender aan
+            MulticastSender.sendMulticast(multicastNode.getName(), multicastNode.getIpAddress());
+        } catch (Exception e) {
+            System.err.println("⚠️ Fout bij multicast versturen:");
+            e.printStackTrace();
+        }
     }
-
 }
+
