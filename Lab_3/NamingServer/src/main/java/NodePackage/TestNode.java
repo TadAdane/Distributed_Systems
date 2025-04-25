6c9031c8d48b616040fe571fe5dae546a2810fb6
chrisElh <christian-eh@hotmail.com>
2025-04-25 02:13:25 +0200

    torture

--- Lab_3/NamingServer/src/main/java/NodePackage/TestNode.java
+++ Lab_3/NamingServer/src/main/java/NodePackage/TestNode.java
@@ -3,11 +3,6 @@ package NodePackage;
 public class TestNode {
     public static void main(String[] args) {
         NodeApp app = new NodeApp();
-
-        // 👇 Deze node multicast zichzelf en start de receiver
         app.createAndAnnounceNewNode("nodeX", "192.168.0.77", 5000, 10000);
-
-        // of gewoon een luisterende node
-        // app.createListeningNode("nodeA", "192.168.0.55", 3000, 7000);
     }
 }
